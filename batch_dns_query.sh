16a8f83a63fbd5e9dbcabf134d5656a8362ab804
Stevolus <25365857+Stevolus@users.noreply.github.com>
2024-09-25 16:43:56 +0200

    Update batch_dns_query.sh

--- batch_dns_query.sh
+++ batch_dns_query.sh
@@ -2,14 +2,20 @@
 
 # Description: Ridiculously simple Batch DNS Checker. Just shows how lazy I am.
 # Author: Steven Marchant
-# Notes: Add your own list of DNS server IPs next to 'dns_servers'
-#        add the fqdn of the server you want to query next to 'query'.
+# Notes: Expects there to be a text file named domains.txt consisting of the domains
+# you want to query. The servers variable is to change the DNS servers to any custom ones you may want to test.
+
+servers='1.1.1.1 8.8.8.8'
+
+while read p; do
+  nslookup "$p" "$servers"
+done <domains.txt
 
 
-dns_servers='1.1.1.1 8.8.8.8'
-query='google.com'
+#dns_servers='1.1.1.1 8.8.8.8'
+#query='google.com'
 
-for i in $dns_servers
-do
-   nslookup $query $i
-done
+#for i in $dns_servers
+#do
+#   nslookup $query $i
+#done
